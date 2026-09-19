9b5eefb0c3067773c56b1569da38789e7ccf622a
anilbhu10081996-prog <anilbhu10081996@gmail.com>
2026-09-20 01:45:18 +0530

    Use environment variable for server port

--- client/client/admin/server/server.js
+++ client/client/admin/server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
